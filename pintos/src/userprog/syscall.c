3043c1a11457dc01f702516e071d6025a891519b
Run Yu Zhang <runyuzhang@berkeley.edu>
2015-11-03 22:48:26 +0000

    lol

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -28,6 +28,9 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   uint32_t* args = ((uint32_t*) f->esp);
+  if (args[0] == SYS_PRACTICE) {
+    f->eax = args[1] + 1;
+  }
   //printf("System call number: %d\n", args[0]);
   if (args[0] == SYS_EXIT) {
     f->eax = args[1];
